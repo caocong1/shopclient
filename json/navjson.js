6793a4c4dfd6905db5269346298c8fa7abdd648c
caocong <caocong1@gmail.com>
2017-08-25 18:39:05 +0800

    s

---
+++ json/navjson.js
@@ -0,0 +1,62 @@
+nav =
+    [
+        {
+            "id": 1,
+            "navname": "通知公告",
+            "status": 1,
+            "icon": "fa-envelope-open",
+            "page": "notice"
+        },
+        {
+            "id": 2,
+            "navname": "通知单",
+            "status": 1,
+            "icon": "fa-newspaper-o",
+            "page": "statement"
+        },
+        {
+            "id": 3,
+            "navname": "店铺销售",
+            "status": 1,
+            "icon": "fa-shopping-bag",
+            "subnav": [
+                {
+                    "id": 4,
+                    "navname": "销售报表",
+                    "status": 1,
+                    "icon": "fa-bar-chart",
+                    "page": "chart"
+                },
+                {
+                    "id": 5,
+                    "navname": "销售补录",
+                    "status": 1,
+                    "icon": "fa-jpy",
+                    "page": "adjust"
+                }
+            ]
+        },
+        {
+            "id": 6,
+            "navname": "系统管理",
+            "status": 1,
+            "icon": "fa-android",
+            "subnav": [
+                {
+                    "id": 7,
+                    "navname": "店铺人员管理",
+                    "status": 1,
+                    "icon": "fa-users",
+                    "page": "person"
+                },
+                {
+                    "id": 8,
+                    "navname": "系统设置",
+                    "status": 1,
+                    "icon": "fa-cog",
+                    "page": "system"
+                }
+            ]
+        }
+    ]
+;
